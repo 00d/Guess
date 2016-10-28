c734e51abafd1267c2caf463d58231d335efe731
Johnny B <Johnny B>
2016-10-28 14:51:18 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/thaumatech/jbritzke/guess/Guess.java
@@ -0,0 +1,13 @@
+package com.thaumatech.jbritzke.guess;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class Guess extends ApplicationTestCase<Application> {
+    public Guess() {
+        super(Application.class);
+    }
+}
